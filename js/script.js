1fd05092152c0217133fee6cc1c30b56fcc09f99
Jose Alejandro Tamayo <jalejandrotamayo@hotmail.com>
2014-12-05 15:14:50 -0600

    blog51212

--- js/script.js
+++ js/script.js
@@ -1,52 +1,77 @@
 // Javascript Code.
 $(document).ready(function () {
-  // $.get("_posts/2014-11-09.json", function(data){
-  //
-  //   var posts = JSON.parse(data);
-  //   $(".posts").html(posts['posts'][0]['title']);
-  //
-  // });
-
-  $(this).load("_posts/posts.json", function(data){
-    var posts = JSON.parse(data);
-    $(".posts").html(posts['posts'][0]['title']);
-    //
-
-  });
-
-
-    $(document).ready(function() {
-    $('.boton').click(function() {
-        $('.ejemplo').fadeOut('slow');
-    });
+// $.get("_posts/2014-11-09.json", function(data){
+//
+// var posts = JSON.parse(data);
+// $(".posts").html(posts['posts'][0]['title']);
+//
+// });
+$(this).load("_posts/posts.json", function(data){
+var posts = JSON.parse(data);
+$(".posts").html(posts['posts'][0]['content']);
+$(".posts2").html(posts['posts'][1]['content']);
+$(".posts3").html(posts['posts'][2]['content']);
+//
+//*****************************************************************************
+//************************** ocultar cuerpo ***********************************
 });
-
-//******************************************************************************
-//*****************************PROBANDO*****************************************
 $(document).ready(function() {
-   $('.post').mouseenter(function() {
-       $(this).animate({
-           height: '+=10px'
-       });
-   });
-   $('.post').mouseleave(function() {
-       $(this).animate({
-           height: '-=10px'
-       }); 
-   });
-   $('.post').click(function() {
-       $(this).toggle(1000);
-   }); 
+$('.boton').click(function() {
+$('.cuerpo').toggle('slow');
+});
 });
 //******************************************************************************
+//*************************** boton 2 ******************************************
+$(document).ready(function() {
+$('.boton2').click(function() {
+$('.cuerpo').toggle('fast');
+$('.posts').show('fast');
+});
+});
+//******************************************************************************
+//*************************** boton 3 ******************************************
+$(document).ready(function() {
+$('.boton3').click(function() {
+$('.cuerpo').toggle('fast');
+$('.posts').show('fast');
+});
+});
+//******************************************************************************
+//*************************** boton 4 ******************************************
+$(document).ready(function() {
+$('.boton4').click(function() {
+$('.cuerpo').toggle('fast');
+$('.posts').show('fast');
+});
+});
+//******************************************************************************
+//***************************** boton older post********************************
 
 $(document).ready(function() {
-    $('div.menu>div').hide();
-  })
-//******************************************************************************
-//******************************************************************************
-
-
-
+$('.post').mouseenter(function() {
+$(this).animate({
+height: '+=10px'
+});
+});
+$('.post').mouseleave(function() {
+$(this).animate({
+height: '-=10px'
+});
+});
+$('.post').click(function() {
+$(this).toggle(1000);
+});
 });
 
+
+//******************************************************************************
+//******************************** PROBANDO*************************************
+$(document).ready(function() {
+$('div.menu>div').hide();
+})
+
+
+
+//******************************************************************************
+//******************************************************************************
+});
